2bdc3ab0f2b5df514d807241f543020749894b92
abdulmalek <abdulmalek.esughi@ynov.com>
2024-10-27 17:34:35 +0100

    finshed choose page and Go file works

--- main.go
+++ main.go
@@ -2,28 +2,44 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
-	"os"
 	"text/template"
 )
 
-func main() {
-	temp, tempErr := template.ParseGlob("Templates/*.html")
-	if tempErr != nil {
-		fmt.Printf("Error loading templates: %s", tempErr.Error())
-		os.Exit(2)
+var templates *template.Template
+
+func init() {
+	var err error
+	templates, err = template.ParseGlob("Templates/*.html")
+	if err != nil {
+		log.Fatalf("Error loading templates: %v", err)
 	}
-	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
+}
 
-		temp.ExecuteTemplate(w, "Home", nil)
-	})
-	http.HandleFunc("/rules", func(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "index", nil)
+}
 
-		temp.ExecuteTemplate(w, "rules", nil)
-	})
-	http.HandleFunc("/engines", func(w http.ResponseWriter, r *http.Request) {
+func enginesHandler(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "engines", nil)
+}
 
-		temp.ExecuteTemplate(w, "engines", nil)
-	})
-	http.ListenAndServe("localhost:8080", nil)
+func chooseHandler(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "choose", nil)
+}
+
+func main() {
+	http.HandleFunc("/index", homeHandler)
+	http.HandleFunc("/engines", enginesHandler)
+	http.HandleFunc("/choose", chooseHandler)
+
+	fileServer := http.FileServer(http.Dir("./Style"))
+	http.Handle("/Style/", http.StripPrefix("/Style/", fileServer))
+
+	port := ":8080"
+	fmt.Printf("Server starting on port %s...\n", port)
+	if err := http.ListenAndServe(port, nil); err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
 }
