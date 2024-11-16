14edc99bed2378650ee3d644b4b664024a3ddbee
abdulmalek <abdul.esughi@gmail.com>
2024-11-17 00:09:27 +0100

    updated main.go and placed the outputs of the handlers

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module hangmanWeb
 
 go 1.23.0
+
+require hang v0.0.0
+
+replace hang => ./Backend
--- main.go
+++ main.go
@@ -2,55 +2,28 @@ package main
 
 import (
 	"fmt"
+	backend "hang/Core" // Make sure this import path matches your project structure
 	"log"
 	"net/http"
-	"text/template"
 )
 
-var templates *template.Template
-
-func init() {
-	var err error
-	templates, err = template.ParseGlob("Templates/*.html")
-	if err != nil {
-		log.Fatalf("Error loading templates: %v", err)
-	}
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	templates.ExecuteTemplate(w, "index", nil)
-}
-
-func enginesHandler(w http.ResponseWriter, r *http.Request) {
-	templates.ExecuteTemplate(w, "engines", nil)
-}
-
-func chooseHandler(w http.ResponseWriter, r *http.Request) {
-	templates.ExecuteTemplate(w, "choose", nil)
-}
-
-func gameHandler(w http.ResponseWriter, r *http.Request) {
-	templates.ExecuteTemplate(w, "game", nil)
-}
-
-func scoreboardHandler(w http.ResponseWriter, r *http.Request) {
-	templates.ExecuteTemplate(w, "scoreboard", nil)
-}
-
 func main() {
-	http.HandleFunc("/index", homeHandler)
-	http.HandleFunc("/engines", enginesHandler)
-	http.HandleFunc("/choose", chooseHandler)
-	http.HandleFunc("/game", gameHandler)
-	http.HandleFunc("/scoreboard", scoreboardHandler)
+	// Initialize templates first
+	backend.InitTemplates()
 
 	fileServer := http.FileServer(http.Dir("./Style"))
 	http.Handle("/Style/", http.StripPrefix("/Style/", fileServer))
 
-	var err error = http.ListenAndServe(":8080", nil)
-	if err != nil {
+	http.HandleFunc("/start-game", backend.StartGameHandler)
+	http.HandleFunc("/guess", backend.GuessHandler)
+	http.HandleFunc("/index", backend.IndexHandler)
+	http.HandleFunc("/choose", backend.ChooseHandler)
+	http.HandleFunc("/game", backend.GameHandler)
+	http.HandleFunc("/scoreboard", backend.ScoreboardHandler)
+	http.HandleFunc("/engines", backend.EnginesHandler)
+
+	fmt.Printf("Server starting on port 8080...\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
-	} else {
-		fmt.Printf("Server starting on port 8080...\n")
 	}
 }
